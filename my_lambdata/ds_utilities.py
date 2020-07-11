9febd29d9dba865a7b6ee9520a61b45d1c9959ec
MarianVinas <marian-vinas@lambdastudents.com>
2020-07-10 23:39:56 -0700

    Completed initial packaging in class

--- my_lambdata/ds_utilities.py
+++ my_lambdata/ds_utilities.py
@@ -2,5 +2,6 @@ def enlarge(n):
     ''' this function will multiply the input by 100'''
     return n * 100
 
-y = int(input("Choose a number: "))
-print(y, enlarge(y))
+if __name__ == '__main__':
+    y = int(input("Choose a number: "))
+    print(y, enlarge(y))
